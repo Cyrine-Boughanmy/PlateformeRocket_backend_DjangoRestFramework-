2b75c9004f738895fd5f3e057a4ae4498e9a5b4e
Chadha HAJJI <chadha.hadji@gmail.com>
2022-07-20 14:27:34 +0100

    password

--- super_user/models.py
+++ super_user/models.py
@@ -2,9 +2,10 @@
 from django.db import models
 
 from django.dispatch import receiver
-# from django_rest_passwordreset.signals import reset_password_token_created
+
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
+from django_rest_passwordreset.signals import reset_password_token_created
 '''
 This model is for the Super User who can be the ADMIN 
 '''
@@ -26,38 +27,38 @@ class SuperUser(models.Model):
 '''
 Pour la réinitialisation des mots de passe 
 '''
-# @receiver(reset_password_token_created)
-# def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
 
-#     user = SuperUser.objects.filter(
-#         email=reset_password_token.user.email).get()
-#     nom = reset_password_token.user.nom
-#     prenom = reset_password_token.user.prenom
+    user = SuperUser.objects.filter(
+        email=reset_password_token.user.email).get()
+    nom = reset_password_token.user.nom
+    prenom = reset_password_token.user.prenom
 
-#     email_plaintext_message = """Bonjour {} {},
+    email_plaintext_message = """Bonjour {} {},
 
-# Vous avez récemment demandé la réinitialisation du mot de passe de votre compte Markus. 
-# Veuillez suivre la procédure communiquée ci-dessous.
+Vous avez récemment demandé la réinitialisation du mot de passe de votre compte Markus. 
+Veuillez suivre la procédure communiquée ci-dessous.
 
-# Copiez ce code de modification : {}
+Copiez ce code de modification : {}
 
-# Ensuite, revenez sur la page de modification du mot de passe de l’application et renseignez le code reçu par mail dans le champs correspondant.
+Ensuite, revenez sur la page de modification du mot de passe de l’application et renseignez le code reçu par mail dans le champs correspondant.
 
-# Enfin, choisissez votre nouveau mot de passe et confirmez-le.
+Enfin, choisissez votre nouveau mot de passe et confirmez-le.
 
-# Si vous n’avez pas demandé une réinitialisation de votre mot de passe, ignorez ce email.
+Si vous n’avez pas demandé une réinitialisation de votre mot de passe, ignorez ce email.
 
-# Markus vous remercie de votre confiance.
+Markus vous remercie de votre confiance.
 
-#     """.format(nom, prenom, reset_password_token.key)
+    """.format(nom, prenom, reset_password_token.key)
 
-#     send_mail(
-#         # title:
-#         "Password Reset for {title}".format(title="Rocket Coding Bootcamp"),
-#         # message:
-#         email_plaintext_message,
-#         # from:
-#         "rocketcoding.bootcamp@gmail.com",
-#         # to:
-#         [reset_password_token.user.email]
-#     )
+    send_mail(
+        # title:
+        "Password Reset for {title}".format(title="Rocket Coding Bootcamp"),
+        # message:
+        email_plaintext_message,
+        # from:
+        "rocketcoding.bootcamp@gmail.com",
+        # to:
+        [reset_password_token.user.email]
+    )
